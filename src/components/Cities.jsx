95e8618db0848b897b8ff10c2f40880c9efd4a1c
Marcel Bosch <marcel@ES2BT-M0Z30HHLV.local>
2022-07-01 09:44:20 +0200

    changed get url name

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom'
 
 const Cities = () => {
   const location = useLocation();
-  const cityName = location.pathname.slice(8);
+  const cityName = location.pathname.slice(19);
   return (
     <div className='city'>
       <City name={cityName} />
